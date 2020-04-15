e1ae4d8b44a508d2f2faab4009bcc6f197bc1ea7
Amit Kumar <amitkumar.mdp@gmail.com>
2020-04-16 00:25:23 +0530

    Dynamically allocate categories to Category

--- onlinebookstore/src/main/java/com/ak/book/onlinebookstore/OnlinebookstoreApplication.java
+++ onlinebookstore/src/main/java/com/ak/book/onlinebookstore/OnlinebookstoreApplication.java
@@ -2,9 +2,7 @@ package com.ak.book.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan(basePackages="com.ak.book.onlinebookstore.*")
 @SpringBootApplication
 public class OnlinebookstoreApplication {
 
--- onlinebookstore/src/main/java/com/ak/book/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/com/ak/book/onlinebookstore/config/RepositoryConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer{
@@ -18,5 +19,10 @@ public class RepositoryConfig implements RepositoryRestConfigurer{
 		config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
 				.map(Type::getJavaType)
 				.toArray(Class[]::new));
+		
+		//global mapping for all the incoming request 
+		config.getCorsRegistry()
+			.addMapping("/**")
+			.allowedOrigins("http://localhost:4200");
 	}
 }
