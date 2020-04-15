c62b21d2dd8c9d3adcb564c681aeeb27c83b0c11
Amit Kumar <amitkumar.mdp@gmail.com>
2020-04-16 00:41:56 +0530

    Category Updated

--- onlinebookstore/src/main/java/com/ak/book/onlinebookstore/OnlinebookstoreApplication.java
+++ onlinebookstore/src/main/java/com/ak/book/onlinebookstore/OnlinebookstoreApplication.java
@@ -2,9 +2,8 @@ package com.ak.book.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan(basePackages="com.ak.book.onlinebookstore.*")
+
 @SpringBootApplication
 public class OnlinebookstoreApplication {
 
